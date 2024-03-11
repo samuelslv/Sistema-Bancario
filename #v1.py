c972c8084a208e760f3ecc838932abefa7e6ed48
Samuel Silva <samuelslv7@gmail.com>
2024-03-11 15:49:47 -0300

    menu basico

--- #v1.py
+++ #v1.py
@@ -1,1 +1,37 @@
-#v1.0 do Sistema Bancario
+# v1.0 do Sistema Bancario
+
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+    if opcao == "d":
+        print("DEPOSITO")
+
+
+    elif opcao == "s":
+        print("SAQUE")
+
+
+    elif opcao == "e":
+        print("EXTRATO")
+
+
+    elif opcao == "q":
+        break
+
+    
+    else:
+        print("Tente novamente")
